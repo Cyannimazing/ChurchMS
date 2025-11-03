127a9a8e20152c15efa8ffb69d26356c71cb63f8
Cyann <cyrilnarvasa589@gmail.com>
2025-11-03 23:24:17 +0800

    ADDED UPDATE

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,11 +1,1 @@
-import Axios from "axios";
-
-const axios = Axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "https://churchms-d9h3.onrender.com",
-  withCredentials: true,
-  headers: {
-    "X-Requested-With": "XMLHttpRequest",
-  },
-});
-
-export default axios;
+import Axios from "axios"; const axios = Axios.create({ baseURL: process.env.NEXT_PUBLIC_BACKEND_URL, headers: { "X-Requested-With": "XMLHttpRequest", }, withCredentials: true, withXSRFToken: true, }); export default axios;
